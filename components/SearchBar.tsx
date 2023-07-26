36f659e9a2c9474bb2b16686900623bcae94e4c0
mateuszNadolny <mateusznadolny00@gmail.com>
2023-07-26 21:11:40 +0200

    initial commit

---
+++ components/SearchBar.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useState } from 'react'
+
+import { SearchManufacturer } from './index'
+
+const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState('')
+
+    const handleSearch = () => {}
+
+    return (
+        <form onSubmit={handleSearch} className="searchbar">
+            <div className="searchbar__item">
+                <SearchManufacturer
+                    manufacturer={manufacturer}
+                    setManufacturer={setManufacturer}
+                />
+            </div>
+        </form>
+    )
+}
+
+export default SearchBar
---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Combobox, Transition } from '@headlessui/react'
+
+import { SearchManufacturerProps } from '@/types'
+
+const SearchManufacturer = ({
+    manufacturer,
+    setManufacturer,
+}: SearchManufacturerProps) => {
+    return <div>SearchManufacturer</div>
+}
+
+export default SearchManufacturer
