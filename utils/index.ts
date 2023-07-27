b024e77bcd3143c497731beaf415cd6d865cd52f
mateuszNadolny <mateusznadolny00@gmail.com>
2023-07-27 11:53:43 +0200

    created new utility function for dispalying the rent price

--- utils/index.ts
+++ utils/index.ts
@@ -10,11 +10,24 @@ export async function fetchCars() {
     }
 
     const response = await fetch(
-        'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+        'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
         { headers: headers }
     )
 
     const result = response.json()
 
     return result
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50
+    const mileageFactor = 0.1
+    const ageFactor = 0.05
+
+    const mileageRate = city_mpg * mileageFactor
+    const ageRate = (new Date().getFullYear() - year) * ageFactor
+
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
+
+    return rentalRatePerDay.toFixed(0)
 }
