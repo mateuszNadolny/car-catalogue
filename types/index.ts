c6c06d04d6b37a7b23b4842f1c36d2911bf87975
mateuszNadolny <mateusznadolny00@gmail.com>
2023-07-27 11:53:10 +0200

    added new interface for car card component

--- types/index.ts
+++ types/index.ts
@@ -3,6 +3,9 @@ import { MouseEventHandler } from 'react'
 export interface CustomButtonProps {
     title: string
     containerStyles?: string
+    textStyles?: string
+    rightIcon?: string
+    isDisabled?: boolean
     handleClick?: MouseEventHandler<HTMLButtonElement>
     btnType?: 'button' | 'submit'
 }
@@ -10,4 +13,40 @@ export interface CustomButtonProps {
 export interface SearchManufacturerProps {
     manufacturer: string
     setManufacturer: (manufacturer: string) => void
+}
+
+export interface CarProps {
+    car: {
+        city_mpg: number
+        class: string
+        combination_mpg: number
+        cylinders: number
+        displacement: number
+        drive: string
+        fuel_type: string
+        highway_mpg: number
+        make: string
+        model: string
+        transmission: string
+        year: number
+    }
+}
+
+export interface CarDetailsProps {
+    car: {
+        city_mpg: number
+        class: string
+        combination_mpg: number
+        cylinders: number
+        displacement: number
+        drive: string
+        fuel_type: string
+        highway_mpg: number
+        make: string
+        model: string
+        transmission: string
+        year: number
+    }
+    isOpen: boolean
+    closeModal: () => void
 }
